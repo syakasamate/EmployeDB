dced3c7a355a5525e91850c97232e5fc88d59eee
syakasamate <samate015@gmail.com>
2019-09-10 03:16:47 +0000

    menu

---
+++ menu.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="style/bootstrap.min.css">
+    <link rel="stylesheet" href="style/js.css">
+
+</head>
+<body>
+    <div class="nav navbar-dark bg-primary">
+    <div class="container-fluid">
+     <ul class="nav navbar-nav">
+     <li><a href="">Acceuil</a></li>
+     <li><a href="?page=add">Ajout des Employes</a></li>
+     <li><a href="?page=liste">Liste des Employe</a></li>
+     </ul>
+    </div>
+    </div>
+</body>
+</html>
